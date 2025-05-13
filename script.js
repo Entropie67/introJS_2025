4c5adc60e185bc74a5b0a59bf6a0f52de9efe70b
Entropie67 <elophe.olivier@gmail.com>
2025-05-13 09:54:24 +0200

    Affichage d'un dos de carte cliquable

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 
-
-let btn = document.querySelector('#btn');
-
-btn.addEventListener('click', (e) => {
-    let img = document.querySelector('#lss');
-    if (img.style.display == 'none') {
-        img.style.display = 'block';
-    }else{
-        img.style.display = 'none';
-    }
-});
-// ------------------------
-
 let boite = document.querySelector('#images');
 
 for (let i = 0; i < 12; i++) {
-    boite.innerHTML  += `<h2>Image ${i}</h2>`;
-    boite.innerHTML  += `<img src="images/${i}.jpg">`;
+    let img = document.createElement('img');
+    img.src = 'images/1.jpg';
+    img.id = `img${i}`;
+    img.addEventListener('click', () => {
+        alert("bip");
+    });
+    boite.appendChild(img);
 }
+
+
