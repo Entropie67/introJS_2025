3b6123fae8f3a898541524738701310fdd69332e
Entropie67 <elophe.olivier@gmail.com>
2025-05-15 13:58:06 +0200

    deuxième clicage bloqué

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ for (let i = 0; i < 5; i++) {
     img.id = `img${i}`;
     img.classList.add('cards');
     img.addEventListener('click', () => {
-        let nombre = Math.floor(Math.random() * 10) + 1;
-        console.log(nombre);
-        img.src = `images/${nombre}.png`;
+        if (img.src.includes('images/dos.png')) {
+            let nombre = Math.floor(Math.random() * 10) + 1;
+            console.log(nombre);
+            img.src = `images/${nombre}.png`;
+        }else {
+            alert('tricheur');
+        }
 
     });
     boite.appendChild(img);
