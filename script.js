a93aab91d806fbd700bed28dbf1dbb652ce4a06a
Entropie67 <elophe.olivier@gmail.com>
2025-05-15 13:21:57 +0200

    cartes avec le bon dos

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 let boite = document.querySelector('#images');
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 5; i++) {
     let img = document.createElement('img');
-    img.src = 'images/1.jpg';
+    img.src = 'images/dos.png';
     img.id = `img${i}`;
+    img.classList.add('cards');
     img.addEventListener('click', () => {
         let nombre = Math.floor(Math.random() * 10) + 1;
         console.log(nombre);
