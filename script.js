f2bd9248982d934f97d34f355454263bfdf58f88
Entropie67 <elophe.olivier@gmail.com>
2025-05-19 10:49:53 +0200

    Possibilité de rejouer

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-
-let boite = document.querySelector('#images');
-let compteur = 0;
-let cartes = []; // tableau vide
-for (let i = 0; i < 5; i++) {
-    let img = document.createElement('img');
-    img.src = 'images/dos.png';
-    img.id = `img${i}`;
-    img.classList.add('cards');
-    img.classList.add(i);
-    img.addEventListener('click', () => {
-        if (img.src.includes('images/dos.png')) {
-            let nombre = Math.floor(Math.random() * 10) + 1;
-            cartes.push(nombre); // On ajoute la carte
-            console.log(cartes);
-            img.src = `images/${nombre}.png`;
-            compteur++;
-            if (compteur == 5){
-                let v = verif(cartes);
-                console.log(v);
-                let ga = document.querySelector('#gagnante');
-                ga.innerHTML = `<img class="cards" src="images/${v}.png">`
+const start = () => {
+    console.log('start');
+    document.querySelector('#images').innerHTML = '';
+    let boite = document.querySelector('#images');
+    let compteur = 0;
+    let cartes = []; // tableau vide
+    for (let i = 0; i < 5; i++) {
+        let img = document.createElement('img');
+        img.src = 'images/dos.png';
+        img.id = `img${i}`;
+        img.classList.add('cards');
+        img.classList.add(i);
+        img.addEventListener('click', () => {
+            if (img.src.includes('images/dos.png')) {
+                let nombre = Math.floor(Math.random() * 10) + 1;
+                cartes.push(nombre); // On ajoute la carte
+                console.log(cartes);
+                img.src = `images/${nombre}.png`;
+                compteur++;
+                if (compteur == 5){
+                    let v = verif(cartes);
+                    console.log(v);
+                    let ga = document.querySelector('#gagnante');
+                    ga.innerHTML += `<img class="cards" src="images/${v}.png">`
+                }
+            }else {
+                alert('Tricheur !');
             }
-        }else {
-            alert('Tricheur !');
-        }
-    });
-    boite.appendChild(img);
+        });
+        boite.appendChild(img);
+    }
 }
+
 
 const verif = (tab) => {
         let tabcompteur = [0, 0, 0, 0, 0];
@@ -43,5 +47,10 @@ const verif = (tab) => {
         return false;
 }
 
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    //location.reload();
+    start();
+});
 
 
