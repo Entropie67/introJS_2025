4f005feed43fea372edfbdc70c2058c22483b017
Entropie67 <elophe.olivier@gmail.com>
2025-05-19 11:25:16 +0200

    Version avec étoiles et plus de personnages

---
+++ data.js
@@ -0,0 +1,102 @@
+const personnages = {
+  "1": {
+    "nom": "Adriana Lima",
+    "chemin": "images/Adriana Lima.png",
+    "etoiles": 5
+  },
+  "2": {
+    "nom": "Agatha Harkness",
+    "chemin": "images/Agatha Harkness.png",
+    "etoiles": 4
+  },
+  "3": {
+    "nom": "Akame",
+    "chemin": "images/Akame.png",
+    "etoiles": 4
+  },
+  "4": {
+    "nom": "Akeno Himejima",
+    "chemin": "images/Akeno Himejima.png",
+    "etoiles": 3
+  },
+  "5": {
+    "nom": "Albedo",
+    "chemin": "images/Albedo.png",
+    "etoiles": 2
+  },
+  "6": {
+    "nom": "Alma Judikhali",
+    "chemin": "images/Alma Judikhali.png",
+    "etoiles": 2
+  },
+  "7": {
+    "nom": "Alpha",
+    "chemin": "images/Alpha.png",
+    "etoiles": 2
+  },
+  "8": {
+    "nom": "Anita",
+    "chemin": "images/Anita.png",
+    "etoiles": 4
+  },
+  "9": {
+    "nom": "Anna Liebert",
+    "chemin": "images/Anna Liebert.png",
+    "etoiles": 2
+  },
+  "10": {
+    "nom": "antman",
+    "chemin": "images/antman.png",
+    "etoiles": 1
+  },
+  "11": {
+    "nom": "Aphrodite",
+    "chemin": "images/Aphrodite.png",
+    "etoiles": 5
+  },
+  "12": {
+    "nom": "arlechino",
+    "chemin": "images/arlechino.png",
+    "etoiles": 1
+  },
+  "13": {
+    "nom": "Asuna",
+    "chemin": "images/Asuna.png",
+    "etoiles": 3
+  },
+  "14": {
+    "nom": "Athéna",
+    "chemin": "images/Athéna.png",
+    "etoiles": 4
+  },
+  "15": {
+    "nom": "Azula",
+    "chemin": "images/Azula.png",
+    "etoiles": 5
+  },
+  "16": {
+    "nom": "Batgirl",
+    "chemin": "images/Batgirl.png",
+    "etoiles": 4
+  },
+  "17": {
+    "nom": "Beta",
+    "chemin": "images/Beta.png",
+    "etoiles": 3
+  },
+  "18": {
+    "nom": "Black Cat",
+    "chemin": "images/Black Cat.png",
+    "etoiles": 5
+  },
+  "19": {
+    "nom": "Boa Hancock",
+    "chemin": "images/Boa Hancock.png",
+    "etoiles": 5
+  },
+  "20": {
+    "nom": "Bruce Banner",
+    "chemin": "images/Bruce Banner.png",
+    "etoiles": 3
+  }
+}
--- script.js
+++ script.js
@@ -1,55 +1,72 @@
 const start = () => {
     console.log('start');
     document.querySelector('#images').innerHTML = '';
-    let boite = document.querySelector('#images');
+    document.querySelector('#gagnante').innerHTML = '<h2>Cartes gagnées</h2>';
+
+    const boite = document.querySelector('#images');
+    const cles = Object.keys(personnages);
     let compteur = 0;
-    let cartes = []; // tableau vide
+    let cartes = [];
+
     for (let i = 0; i < 5; i++) {
-        let img = document.createElement('img');
+        const container = document.createElement('div');
+        container.classList.add('card-container');
+
+        const img = document.createElement('img');
         img.src = 'images/dos.png';
-        img.id = `img${i}`;
         img.classList.add('cards');
-        img.classList.add(i);
+        img.id = `img${i}`;
+
+        const stars = document.createElement('div');
+        stars.classList.add('stars');
+        stars.innerHTML = ''; // vide au départ
+
         img.addEventListener('click', () => {
             if (img.src.includes('images/dos.png')) {
-                let nombre = Math.floor(Math.random() * 10) + 1;
-                cartes.push(nombre); // On ajoute la carte
-                console.log(cartes);
-                img.src = `images/${nombre}.png`;
+                const randomKey = cles[Math.floor(Math.random() * cles.length)];
+                const perso = personnages[randomKey];
+
+                cartes.push(randomKey);
+                img.src = perso.chemin;
+                img.title = `${perso.nom} (${perso.etoiles}★)`;
+
+                // Ajouter les étoiles visuelles
+                stars.innerHTML = '★'.repeat(perso.etoiles) + '☆'.repeat(5 - perso.etoiles);
+
                 compteur++;
-                if (compteur == 5){
-                    let v = verif(cartes);
-                    console.log(v);
-                    let ga = document.querySelector('#gagnante');
-                    ga.innerHTML += `<img class="cards" src="images/${v}.png">`
+                if (compteur === 5) {
+                    const gagnant = verif(cartes);
+                    if (gagnant) {
+                        const persoGagnant = personnages[gagnant];
+                        const imgGagnante = document.createElement('div');
+                        imgGagnante.classList.add('card-container');
+                        imgGagnante.innerHTML = `
+                            <img class="cards" src="${persoGagnant.chemin}" title="${persoGagnant.nom} (${persoGagnant.etoiles}★)">
+                            <div class="stars">★`.repeat(persoGagnant.etoiles) + `☆`.repeat(5 - persoGagnant.etoiles) + `</div>`;
+                        document.querySelector('#gagnante').appendChild(imgGagnante);
+                    }
                 }
-            }else {
+            } else {
                 alert('Tricheur !');
             }
         });
-        boite.appendChild(img);
-    }
-}
 
+        container.appendChild(img);
+        container.appendChild(stars);
+        boite.appendChild(container);
+    }
+};
 
 const verif = (tab) => {
-        let tabcompteur = [0, 0, 0, 0, 0];
-        for (let i = 0; i < tab.length; i++) {
-            for (let j = 0; j < tab.length; j++) {
-                if (tab[j] == tab[i]){
-                    tabcompteur[i] += 1;
-                    if (tabcompteur[i] >= 3){
-                        return tab[i];
-                    }
-                }
-            }
-        }
-        return false;
-}
+    const compte = {};
+    for (let id of tab) {
+        compte[id] = (compte[id] || 0) + 1;
+        if (compte[id] >= 3) return id;
+    }
+    return false;
+};
 
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-    //location.reload();
+document.querySelector('#btn').addEventListener('click', () => {
     start();
 });
 
